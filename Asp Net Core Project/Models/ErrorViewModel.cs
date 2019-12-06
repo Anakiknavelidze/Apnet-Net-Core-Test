86752eeaa2a0e099c92bdc141f6c93901bcdc394
Albert Giulbaziani <albert.giulbaziani@lb.ge>
2019-12-06 16:30:24 +0400

    first commit

---
+++ Asp Net Core Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp_Net_Core_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
